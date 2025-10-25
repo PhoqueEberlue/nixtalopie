01d7950b0ea9b40d9b1ffe0e80f9ca9d0890a574
PhoqueEberlue <andrewmhdb@gmail.com>
2025-10-26 00:00:24 +0200

    added chinese input method

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -27,6 +27,32 @@ in
     '';
   }; 
 
+  # Enabling Chinese input method
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-chinese-addons
+        fcitx5-pinyin-zhwiki
+        # TODO: theme not working, probably should be activated somehow
+        fcitx5-tokyonight
+      ];
+      settings.inputMethod = {
+        GroupOrder."0" = "Default";
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us";
+          DefaultIM = "pinyin";
+        };
+        "Groups/0/Items/0".Name = "keyboard-us";
+        "Groups/0/Items/1".Name = "pinyin";
+      };
+    };
+  };
+
   home.packages = with pkgs; [
     # Utils
     lshw
